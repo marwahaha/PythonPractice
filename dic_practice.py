87a84bc54d777e6700b513f77bc48c979f75bf3f
devsda12 <jmbalster@hotmail.com>
2018-03-05 18:21:08 +0100

    Creation of the dic_practice to practice dictionary

---
+++ dic_practice.py
@@ -0,0 +1,12 @@
+#Dictionary practice
+
+dic1 = {"Age" : 20, "Class" : 1}
+print(dic1["Age"])
+
+
+name = input("Give your name: ")
+age = input("Give your age: ")
+
+dic2 = {name : age}
+
+print(dic2[name])
