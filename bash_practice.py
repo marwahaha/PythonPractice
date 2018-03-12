43896e7be351075810c20edd5c4955c7f07f29a8
jasper <jmbalster@hotmail.com>
2018-03-12 15:15:16 +0100

    Bash practice added

--- bash_practice.py
+++ bash_practice.py
@@ -1,2 +1,6 @@
 import subprocess
 
+
+image_name = input("Give directory of the image: ")
+subprocess.call(["sudo", "mkdir", "/mnt/temp_image_mount"])
+subprocess.call(["sudo", "mount", "-o", "ro", image_name, "/mnt/temp_image_mount"])
