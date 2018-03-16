468e405092584ec485bd90ba6cb68663d2910b48
jasper <jmbalster@hotmail.com>
2018-03-16 13:55:40 +0100

    Tested hex creator succesfully

--- hex_practice2.py
+++ hex_practice2.py
@@ -3,4 +3,5 @@ import binascii
 file = input("Give file input: ")
 with open(file, 'rb') as f:
     content = f.read()
-print(binascii.hexlify(content))
+var = binascii.hexlify(content)
+print(var[0:40])
