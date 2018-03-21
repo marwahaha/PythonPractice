5d7eacdc69e2e3d48f31686c43e89c1f403250a0
devsda12 <jmbalster@hotmail.com>
2018-03-21 14:02:36 +0100

    Made a string from output

--- hex_practice2.py
+++ hex_practice2.py
@@ -3,5 +3,5 @@ import binascii
 file = input("Give file input: ")
 with open(file, 'rb') as f:
     content = f.read()
-var = binascii.hexlify(content)
-print(var[0:40])
+var = str(binascii.hexlify(content))
+print(var[2:40])
