9bd690177ff49f47593d14953aff3bb484b0d0db
devsda12 <jmbalster@hotmail.com>
2018-03-21 15:35:49 +0100

    New hex practice to make hex header dic

--- hex_practice3.py
+++ hex_practice3.py
@@ -1,9 +1,26 @@
 import binascii
+import os
 
-hex_headers = {""}
+dir_input = input("Give directory input: ")
+test_file = open("D:\Documents\gen_dict.txt", "w")
+test_file.write("dic = {")
+test_file.close()
 
-file = input("Give file input: ")
-with open(file, 'rb') as f:
-    content = f.read()
-var = str(binascii.hexlify(content))
-print(var[2:40])
+def walkdir(folder):
+    #Walk through each files in a directory
+    for dirpath, dirs, files in os.walk(folder):
+        for filename in files:
+            yield os.path.abspath(os.path.join(dirpath, filename))
+
+for filepath in walkdir(dir_input):
+    filename, file_extension = os.path.splitext(filepath)
+    with open(filepath, 'rb') as f:
+        content = f.read()
+    hex_var = str(binascii.hexlify(content))
+    fh = open("D:\Documents\gen_dict.txt", "a")
+    fh.write("\n  '" + file_extension + "' : '" + hex_var + "',")
+    fh.close()
+
+test2_file = open("D:\Documents\gen_dict.txt", "a")
+test2_file.write("}")
+test2_file.close()
