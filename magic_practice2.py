da80182c32976d127059959e02a6ac8bd46b319e
jasper <jmbalster@hotmail.com>
2018-03-23 15:37:52 +0100

    Magic list maker done

--- magic_practice2.py
+++ magic_practice2.py
@@ -9,16 +9,36 @@ def walkdir(folder):
 
 
 input_dir = input("Give an directory: ")
-output_file = "/home/jasper/output1.txt"
+output_file = "/home/jasper/output2.txt"
 file_create = open(output_file, "w")
 file_create.write("Directory traversed: " + input_dir + "\ndic = {")
 file_create.close()
+output_dic = {}
+counter = 0
 
 for filepath in walkdir(input_dir):
     curr_magic = magic.from_file(filepath, mime=True)
+    filename, file_extension = os.path.splitext(filepath)
+
+    if not str(file_extension):
+        continue
+
+    try:
+        if curr_magic not in output_dic[file_extension]:
+            output_dic[file_extension].append(curr_magic)
+            continue
+    except KeyError:
+        pass
+
+
+    output_dic[file_extension] = [curr_magic]
+
+    print(output_dic)
+    """
     file_write = open(output_file, "a")
-    file_write.write("\n    " + filepath + " >>>> " + curr_magic)
+    file_write.write("\n    '" + file_extension + "': '" + curr_magic + "',")
     file_write.close()
 
 file_write2 = open(output_file, "a")
 file_write2.write("\n}")
+"""
