07655d11e3a5d19aad7b3f274e95f5bd139462d3
devsda12 <jmbalster@hotmail.com>
2018-04-07 16:41:42 +0200

    new lofiles practice file

---
+++ Logfiles practice/logfiles_practice3.py
@@ -0,0 +1,43 @@
+import os
+import magic
+import gzip
+import re
+
+file_check = magic.Magic(mime=True)
+gzip_types = [
+    "application/x-gzip",
+    "application/gzip"
+]
+
+def walkdir(folder):
+    #Walk through each files in a directory
+    for dirpath, dirs, files in os.walk(folder):
+        for filename in files:
+            yield os.path.abspath(os.path.join(dirpath, filename))
+
+#Function that checks logfiles for content
+def logfile_check(dir):
+    failed_logins = []
+    for filepath in walkdir(dir):
+        absolute_path, extension = os.path.splitext(filepath)
+        if file_check.from_file(filepath) == "text/plain":
+            open_file = open(filepath, "r")
+            read_file = open_file.readlines()
+            if "auth" in filepath:
+                for line in read_file:
+                    if re.match("sshd.\*Failed", line):
+                        failed_logins.append(line)
+
+        elif file_check.from_file(filepath) in gzip_types:
+            open_file = gzip.open(filepath, "r")
+            read_file = open_file.readlines()
+            for line in read_file:
+                if "823734bhbf" in str(line):
+                    failed_logins.append(line)
+
+        else:
+            print("Not a log file found at: " + filepath)
+            continue
+    return failed_logins
+input_dir = input("Give an directory: ")
+print(logfile_check(input_dir))
