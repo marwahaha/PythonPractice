62f7e752a8dfc82eb232f8dfba565d64f4607e80
devsda12 <jmbalster@hotmail.com>
2018-04-10 12:10:54 +0200

    vsftpd working

--- Logfiles practice/logfiles_practice3.py
+++ Logfiles practice/logfiles_practice3.py
@@ -22,23 +22,32 @@ def logfile_check(dir):
     failed_logins = []
     for filepath in walkdir(dir):
         absolute_path, extension = os.path.splitext(filepath)
-        if file_check.from_file(filepath) == "text/plain":
+        if "text" in file_check.from_file(filepath):
             open_file = open(filepath, "r")
             read_file = open_file.readlines()
             for line in read_file:
-                if re.match("((.*)[Ff]ailed(.*)[Pp]assword(.*)|(.*)[Pp]assword(.*)[Ff]ailed(.*))", line):
-                    if "auth" in filepath:
+                if "auth" in filepath:
+                    if re.match("((.*)failed(.*)password(.*)|(.*)password(.*)failed(.*))", line, re.IGNORECASE):
                         currdate_time = parser.parse(line[:15])
-                    failed_logins.append([line, currdate_time])
+                        failed_logins.append([line, currdate_time])
+                elif "vsftpd" in filepath:
+                    if re.match("(.*)fail(.*)login(.*)", line, re.IGNORECASE):
+                        currdate_time = parser.parse(line[:23])
+                        print(currdate_time)
+                        failed_logins.append([line, currdate_time])
 
         elif file_check.from_file(filepath) in gzip_types:
             open_file = gzip.open(filepath, "r")
             read_file = open_file.readlines()
             for line in read_file:
-                if re.match("((.*)[Ff]ailed(.*)[Pp]assword(.*)|(.*)[Pp]assword(.*)[Ff]ailed(.*))", str(line)):
-                    if "auth" in filepath:
-                        currdate_time = parser.parse(str(line[:15]))
-                    failed_logins.append([str(line), currdate_time])
+                if "auth" in filepath:
+                    if re.match("((.*)failed(.*)password(.*)|(.*)password(.*)failed(.*))", str(line), re.IGNORECASE):
+                        currdate_time = parser.parse(str(line)[:15])
+                        failed_logins.append([line, currdate_time])
+                elif "vsftpd" in filepath:
+                    if re.match("(.*)fail(.*)login(.*)", str(line), re.IGNORECASE):
+                        currdate_time = parser.parse(str(line)[:23])
+                        failed_logins.append([line, currdate_time])
 
         else:
             print("Not a log file found at: " + filepath)
